226e7f1139f0da4887ee3f2c2ac5b86cb68fa683
Hugo Defrance <hdefrance1@gmail.com>
2013-08-11 18:21:12 +0200

    updated a lot of code in API and added beginning of API doc

--- api/app/controllers/home_controller.rb
+++ api/app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    render :status=>201, :json=>{:message=>"home"}
+    render :status=>200, :json=>{:success=>true, :message=>"API is working properly."}
   end
 end
--- api/app/controllers/tracks_controller.rb
+++ api/app/controllers/tracks_controller.rb
@@ -2,20 +2,20 @@ class TracksController < ApplicationController
   respond_to :json
 
   def   list
-    user = User.find_by_authentication_token(params[:auth_token])
+    user = User.find_by_authentication_token(params[:token])
     if user.nil?
       logger.info("Token not found.")
-      render :status=>404, :json=>{:message=>"Invalid token."}
+      render :status=>403, :json=>{:success:false, :message=>"Invalid token"}
     end
     lst = user.tracks.all()
-    render :status=>201, :json=>{:list=>lst}
+    render :status=>200, :json=>{:success:true, :list=>lst}
   end
 
   def   delete
-    user = User.find_by_authentication_token(params[:auth_token])
+    user = User.find_by_authentication_token(params[:token])
     if user.nil?
       logger.info("Token not found.")
-      render :status=>404, :json=>{:message=>"Invalid token."}
+      render :status=>403, :json=>{:success:false, :error=>"Invalid token"}
     end
     lst = params[:tracks_id]
     counter = 0
@@ -32,21 +32,21 @@ class TracksController < ApplicationController
     if counter > 1
       message = "Element has been deleted"
     end
-    render :status=>201, :json=>{:message=>message}
+    render :status=>200, :json=>{:success:true, :message=>message}
   end
 
-  def   download
+  def download
     track = Track.find_by_id(params[:id])
     if !track.nil?
-      render :status=>404, :json=>{:message=>"The track cannot be found Fuck you"}
+      render :status=>404, :json=>{:success:false, :error=>"The track cannot be found"}
     end
   end
 
-  def   upload
-    user = User.find_by_authentication_token(params[:auth_token])
+  def upload
+    user = User.find_by_authentication_token(params[:token])
     if user.nil?
       logger.info("Token not found.")
-      render :status=>404, :json=>{:message=>"Invalid token."}
+      render :status=>403, :json=>{:success:false, :message=>"Invalid token"}
     end
     hash = {}
     hash[:title] = params[:song].original_filename
@@ -55,6 +55,6 @@ class TracksController < ApplicationController
     if track.nil?
       track.save
     end
-    render :status => 201, :json => {:message => "Song has been uploaded"}
+    render :status => 201, :json => {:success:true, :message => "Song has been uploaded"}
   end
 end
